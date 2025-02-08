cace47a54302196b27eae92e434eca0b21180270
Ahmed Ben Ismael <thenarker20@gmail.com>
2025-02-08 03:23:29 +0200

    edit

--- netlify/functions/admin.js
+++ netlify/functions/admin.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
   const { password } = JSON.parse(event.body);
 
   // Check admin password
-  if (password !== "admin123") {
+  if (password !== process.env.ADMIN_PASSWORD) {
     return {
       statusCode: 401,
       body: JSON.stringify({ error: "Unauthorized" }),
